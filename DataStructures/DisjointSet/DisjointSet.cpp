64752e0cc829a96f69e38a2dcc00b0301ed45e2b
daddudota22 <daddudota22@mail.ru>
2020-12-23 20:14:40 +0700

    m

--- DataStructures/DisjointSet/DisjointSet.cpp
+++ DataStructures/DisjointSet/DisjointSet.cpp
@@ -1,12 +1,42 @@
-#include "../pch.h"
 #include "DisjointSet.h"
+#include <ctime>
 
-
-DisjointSet::DisjointSet()
-{
+DisjointSet::DisjointSet(int max_size) {
+	srand(time(NULL));
+	set = new std::vector<int>(max_size + 1);
+	for (int i = 1; i < max_size + 1; ++i)
+		set->at(i) = i;
 }
 
-
-DisjointSet::~DisjointSet()
-{
+bool DisjointSet::neighbors(int x, int y) {
+	return find(x) == find(y);
 }
+
+void DisjointSet::make_set(int x) {
+	set->at(x) = x;
+}
+
+int DisjointSet::find(int x) {
+	if (set->at(x) == x) return x;
+	set->at(x) = find(set->at(x));
+	return set->at(x);
+}
+
+void DisjointSet::unite(int x, int y) {
+	x = find(x);
+	y = find(y);
+	if (x != y) {
+		if (rand() % 2 == 0)
+			std::swap(x, y);
+		set->at(x) = y;
+	}
+}
+
+bool DisjointSet::is_empty() {
+	return size() == 0;
+}
+
+int DisjointSet::size() {
+	return set->size() - 1;
+}
+
--- DataStructures/DisjointSet/DisjointSet.h
+++ DataStructures/DisjointSet/DisjointSet.h
@@ -1,9 +1,19 @@
 #pragma once
-
+#include <vector>
 
 class DisjointSet {
 public:
-	DisjointSet();
-	~DisjointSet();
+	DisjointSet(int size);
+
+	void unite(int x, int y);
+	void make_set(int x);
+	bool neighbors(int x, int y);
+
+	bool is_empty();
+	int size();
+
+private:
+	std::vector<int>* set;
+	int find(int x);
 };
 
